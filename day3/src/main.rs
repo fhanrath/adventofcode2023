e8b72825cd7a31a456104947e0849d048646cb7f
fenris <fenris@fenris.xyz>
2023-12-05 00:56:11 +0100

    feat: day3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -35,28 +35,74 @@ struct LineItemSymbol {
     pub is_star: bool,
 }
 
+#[derive(Debug, PartialEq)]
+struct CompiledSchematic {
+    items: Vec<CompiledItem>,
+}
+
+#[derive(Debug, PartialEq)]
+enum CompiledItem {
+    PartNumber(PartNumber),
+    Gear(Gear),
+}
+
+#[derive(Debug, PartialEq)]
+struct PartNumber {
+    min_index: usize,
+    max_index: usize,
+    number: usize,
+}
+
+#[derive(Debug, PartialEq)]
+struct Gear {
+    pub index: usize,
+    pub gear_ratio: usize,
+}
+
+impl CompiledSchematic {
+    pub fn get_part_sum(&self) -> usize {
+        self.items
+            .iter()
+            .flat_map(|item| match item {
+                CompiledItem::PartNumber(number) => Some(number.number),
+                CompiledItem::Gear(_) => None,
+            })
+            .sum::<usize>()
+    }
+
+    pub fn get_gear_ratio_sum(&self) -> usize {
+        self.items
+            .iter()
+            .flat_map(|item| match item {
+                CompiledItem::PartNumber(_) => None,
+                CompiledItem::Gear(gear) => Some(gear.gear_ratio),
+            })
+            .sum::<usize>()
+    }
+}
+
 impl Schematic {
-    pub fn get_part_number_sum(&self) -> usize {
-        let mut sum: usize = 0;
+    pub fn compile(&mut self) -> CompiledSchematic {
         let mut second_last_line: Option<&SchematicLine> = None;
         let mut last_line: Option<&SchematicLine> = None;
+        let mut numbers_gears: Vec<Vec<CompiledItem>> = vec![];
         self.lines.iter().for_each(|line| {
             match (last_line, second_last_line) {
                 (None, _) => {}
                 (Some(last_line), second_last_line) => {
-                    let second_last_line_symbol_positions = match second_last_line {
-                        Some(second_last_line) => second_last_line.get_symbol_positions(),
-                        None => vec![],
+                    let second_last_line_items_vec = vec![];
+                    let mut second_last_line_items = &second_last_line_items_vec;
+                    if let Some(second_last_line) = second_last_line {
+                        second_last_line_items = &second_last_line.items;
                     };
-                    let line_symbol_positions = line.get_symbol_positions();
-                    let part_numbers = last_line.get_part_numbers(
-                        &second_last_line_symbol_positions,
-                        &line_symbol_positions,
-                    );
+                    let current_line_items = &last_line.items;
+                    let next_line_items = &line.items;
 
-                    println!("{:?}", part_numbers);
-
-                    sum += part_numbers.iter().sum::<usize>();
+                    numbers_gears.push(get_part_numbers_and_gears(
+                        second_last_line_items,
+                        current_line_items,
+                        next_line_items,
+                    ));
                 }
             }
 
@@ -64,76 +110,131 @@ impl Schematic {
             last_line = Some(line);
         });
 
-        let second_last_line_symbol_positions = second_last_line.unwrap().get_symbol_positions();
+        let second_last_line_items = &second_last_line.unwrap().items;
+        let current_line_items = &last_line.unwrap().items;
+        let empty: Vec<LineItem> = vec![];
 
-        let part_numbers = last_line
-            .unwrap()
-            .get_part_numbers(&second_last_line_symbol_positions, &[]);
+        numbers_gears.push(get_part_numbers_and_gears(
+            second_last_line_items,
+            current_line_items,
+            &empty,
+        ));
 
-        println!("{:?}", part_numbers);
-        sum += part_numbers.iter().sum::<usize>();
+        let numbers_gears: Vec<CompiledItem> = numbers_gears.into_iter().flatten().collect();
 
-        sum
+        CompiledSchematic {
+            items: numbers_gears,
+        }
     }
 }
 
-impl SchematicLine {
-    pub fn get_symbol_positions(&self) -> Vec<usize> {
-        self.items
-            .iter()
-            .flat_map(|item| {
-                if let LineItem::Symbol(symbol) = item {
-                    return Some(symbol.index);
-                }
-                None
-            })
-            .collect()
-    }
+fn get_part_numbers_and_gears(
+    previous_items: &[LineItem],
+    current_items: &[LineItem],
+    next_items: &[LineItem],
+) -> Vec<CompiledItem> {
+    let mut return_value: Vec<CompiledItem> = vec![];
 
-    pub fn get_part_numbers(
-        &self,
-        previous_line_symbol_positions: &[usize],
-        next_line_symbol_positions: &[usize],
-    ) -> Vec<usize> {
-        self.items
-            .iter()
-            .flat_map(|item| {
-                if let LineItem::Number(number) = item {
-                    let mut symbol_positions = self.get_symbol_positions().into_iter();
-                    let mut previous_line_symbol_positions = previous_line_symbol_positions.iter();
-                    let mut next_line_symbol_positions = next_line_symbol_positions.iter();
-                    let min = match number.min_index {
-                        0 => number.min_index,
-                        _ => number.min_index - 1,
-                    };
-                    if symbol_positions
-                        .any(|symbol| symbol >= min && symbol <= (number.max_index + 1))
-                    {
-                        return Some(number.number);
-                    }
-                    if previous_line_symbol_positions
-                        .any(|symbol| symbol >= &min && symbol <= &(number.max_index + 1))
-                    {
-                        return Some(number.number);
-                    }
-                    if next_line_symbol_positions
-                        .any(|symbol| symbol >= &min && symbol <= &(number.max_index + 1))
-                    {
-                        return Some(number.number);
-                    }
+    let (previous_numbers, previous_symbols) = split_numbers_symbols(previous_items);
+    let (current_numbers, current_symbols) = split_numbers_symbols(current_items);
+    let (next_numbers, next_symbols) = split_numbers_symbols(next_items);
+
+    current_items.iter().for_each(|item| match item {
+        LineItem::Number(number) => {
+            let mut current_symbols = current_symbols.iter();
+            let mut previous_symbols = previous_symbols.iter();
+            let mut next_symbols = next_symbols.iter();
+
+            if current_symbols.any(|symbol| is_part(number, symbol)) {
+                return_value.push(CompiledItem::PartNumber(PartNumber {
+                    min_index: number.min_index,
+                    max_index: number.max_index,
+                    number: number.number,
+                }));
+            }
+            if previous_symbols.any(|symbol| is_part(number, symbol)) {
+                return_value.push(CompiledItem::PartNumber(PartNumber {
+                    min_index: number.min_index,
+                    max_index: number.max_index,
+                    number: number.number,
+                }));
+            }
+            if next_symbols.any(|symbol| is_part(number, symbol)) {
+                return_value.push(CompiledItem::PartNumber(PartNumber {
+                    min_index: number.min_index,
+                    max_index: number.max_index,
+                    number: number.number,
+                }));
+            }
+        }
+        LineItem::Symbol(symbol) => {
+            if symbol.is_star {
+                let current_numbers: Vec<usize> = current_numbers
+                    .iter()
+                    .filter(|number| is_part(number, symbol))
+                    .map(|number| number.number)
+                    .collect();
+                let previous_numbers: Vec<usize> = previous_numbers
+                    .iter()
+                    .filter(|number| is_part(number, symbol))
+                    .map(|number| number.number)
+                    .collect();
+                let next_numbers: Vec<usize> = next_numbers
+                    .iter()
+                    .filter(|number| is_part(number, symbol))
+                    .map(|number| number.number)
+                    .collect();
+
+                if current_numbers.len() + previous_numbers.len() + next_numbers.len() == 2 {
+                    let gear_ratio = current_numbers
+                        .into_iter()
+                        .reduce(|acc, item| acc * item)
+                        .unwrap_or(1)
+                        * previous_numbers
+                            .into_iter()
+                            .reduce(|acc, item| acc * item)
+                            .unwrap_or(1)
+                        * next_numbers
+                            .into_iter()
+                            .reduce(|acc, item| acc * item)
+                            .unwrap_or(1);
+
+                    return_value.push(CompiledItem::Gear(Gear {
+                        index: symbol.index,
+                        gear_ratio,
+                    }));
                 }
-                None
-            })
-            .collect()
-    }
+            }
+        }
+    });
+
+    return_value
+}
+
+fn split_numbers_symbols(items: &[LineItem]) -> (Vec<&LineItemNumber>, Vec<&LineItemSymbol>) {
+    let mut numbers: Vec<&LineItemNumber> = vec![];
+    let mut symbols: Vec<&LineItemSymbol> = vec![];
+    items.iter().for_each(|item| match item {
+        LineItem::Number(number) => numbers.push(number),
+        LineItem::Symbol(symbol) => symbols.push(symbol),
+    });
+
+    (numbers, symbols)
+}
+
+fn is_part(number: &LineItemNumber, symbol: &LineItemSymbol) -> bool {
+    symbol.index >= number.min_index && symbol.index <= number.max_index
 }
 
 fn main() -> Result<()> {
     let input = Path::new("./input");
 
-    let schematic = get_schematic(input)?;
+    let mut schematic = get_schematic(input)?;
 
-    println!("Part Sum: {}", schematic.get_part_number_sum());
+    let schematic = schematic.compile();
+
+    println!("Part Sum: {}", schematic.get_part_sum());
+    println!("Gear Score Sum: {}", schematic.get_gear_ratio_sum());
 
     Ok(())
 }
@@ -164,14 +265,17 @@ fn parse_schematic_line(line: &str) -> Result<SchematicLine> {
         .for_each(|(index, char)| match char {
             '0'..='9' => {
                 if number_accumulator.is_empty() {
-                    min_index = index;
+                    min_index = match index {
+                        0 => index,
+                        _ => index - 1,
+                    };
                 }
                 number_accumulator.push(char);
                 if index == line_length - 1 {
                     if let Ok(number) = number_accumulator.parse() {
                         items.push(LineItem::Number(LineItemNumber {
                             min_index,
-                            max_index: index - 1,
+                            max_index: index,
                             number,
                         }));
                     };
@@ -182,7 +286,7 @@ fn parse_schematic_line(line: &str) -> Result<SchematicLine> {
                     if let Ok(number) = number_accumulator.parse() {
                         items.push(LineItem::Number(LineItemNumber {
                             min_index,
-                            max_index: index - 1,
+                            max_index: index,
                             number,
                         }));
                     };
